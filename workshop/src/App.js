15e134059208058d7ccc3564d1eab579f03febf6
GHOSTY <kacperadam10@gmail.com>
2023-05-08 17:54:38 +0200

    some basic changes

--- workshop/src/App.js
+++ workshop/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Projekt na zaliczenie kursu zaawansowanego JS.
+          Projekt na zaliczenie kursu zaawansowanego JS'a.
         </p>
         <a
           className="App-link"
